eaac545dab71945a6e35451c142ddddb47e3db5d
Nicolas Albo <43326612+Ratman305@users.noreply.github.com>
2019-02-05 19:43:42 -0600

    Add files via upload

---
+++ src/clases/Ventana.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ratman305
+ */
+public class Ventana extends JFrame
+{
+    private final int ANCHO = 800, ALTO = 500;
+    private TableroJuego lamina;
+    public Ventana()
+    {
+        setTitle("Pong");
+        setSize(ANCHO, ALTO);
+        setLocationRelativeTo(null); //Ubica la ventana en el centro
+        setResizable(false);
+        lamina = new TableroJuego();
+        add(lamina);
+    }   
+}
